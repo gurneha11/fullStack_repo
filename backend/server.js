2b724a8029fbd449e9c3e3d495c3495c2c5fb7ad
Gurneha <gurneha11@gmail.com>
2026-04-15 19:38:15 +0530

    add root route

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,10 @@ const PORT = process.env.PORT || 5000;
 
 debugger;
 
+app.get("/", (req, res) => {
+  res.send("Backend is running");
+});
+
 app.listen(PORT, () => {
     console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
